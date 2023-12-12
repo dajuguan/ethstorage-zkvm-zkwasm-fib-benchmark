e9580fc31f69317cda7946f1aa7906efa7137ebc
jax <jax@apus.network>
2023-12-12 18:39:05 +0800

    add zkvm code

--- src/fib_go.go
+++ src/fib_go.go
@@ -1,12 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 func main() {
 	var a0, a1 *big.Int
 	a0 = big.NewInt(0)
 	a1 = big.NewInt(1)
-	for i := 2; i <= 100000; i++ {
+	for i := 2; i <= 10000; i++ {
 		a0, a1 = a1, a0.Add(a0, a1)
 	}
 	modResult := a1.Mod(a1, big.NewInt(1<<32))

65105b8caa62c22b04c10ac6ef61fe4d67c2e53f
jax <jax@apus.network>
2023-12-12 15:47:27 +0800

    add zkWasm run command

--- fib/fib_zkgo.go
+++ fib/fib_zkgo.go
@@ -1,5 +1,7 @@
 package main
 
+import "math/big"
+
 //go:wasmimport env wasm_input
 //go:noescape
 func wasm_input(isPublic uint32) uint64
@@ -9,14 +11,15 @@ func wasm_input(isPublic uint32) uint64
 func require(uint32)
 
 func main() {
-	var a0, a1 uint64
-	a0 = wasm_input(1)
-	a1 = wasm_input(1)
-	for i := 2; i <= 1000; i++ {
-		a0, a1 = a1, a0+a1
+	var a0, a1 *big.Int
+	a0 = big.NewInt(0)
+	a1 = big.NewInt(1)
+	for i := 2; i <= 100000; i++ {
+		a0, a1 = a1, a0.Add(a0, a1)
 	}
+	modResult := a1.Mod(a1, big.NewInt(1<<32))
 	an := wasm_input(1)
-	if an != a1 {
+	if an != modResult.Uint64() {
 		require(0)
 	}
 }
